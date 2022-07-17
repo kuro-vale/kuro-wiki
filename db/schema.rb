9a6190498d73f751cd14e08fc987309060bce876
kuro-vale <jsalcedo218@gmail.com>
2022-07-17 23:50:13 +0000

    Add devise gem for authentication

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_16_000338) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_17_225257) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string "username", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["username"], name: "index_users_on_username", unique: true
+  end
 
   create_table "wiki", force: :cascade do |t|
     t.string "title"
