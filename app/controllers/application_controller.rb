3c2088c966f5a0148b99a515e41da7355d1c7647
kuro-vale <jsalcedo218@gmail.com>
2022-07-18 21:23:21 +0000

    Add login and logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   rescue_from ActiveRecord::RecordNotUnique do |e|
     redirect_to wiki_index_url(anchor: 'wikis'), notice: 'Username already taken.'
   end
+
+  rescue_from NoMethodError do |e|
+    redirect_to wiki_index_url(anchor: 'wikis'), notice: 'Invalid login info.'
+  end
 end
