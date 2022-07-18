5ebdb2a5e01034aa83f3bd1e6a23dfdbbf61e9e0
kuro-vale <jsalcedo218@gmail.com>
2022-07-18 02:49:00 +0000

    Add register user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  rescue_from ActiveRecord::RecordNotUnique do |e|
+    redirect_to wiki_index_url(anchor: 'wikis'), notice: 'Username already taken.'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   root 'wiki#index'
+  devise_for :users, skip: :all
+
+  devise_scope :user do
+    post 'auth/register', to: 'devise/registrations#create'
+    post 'auth/login', to: 'devise/sessions#create'
+    delete 'auth/logout', to: 'devise/sessions#destroy'
+  end
 
   resources :wiki
 end
